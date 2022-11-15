c1a60453baae5f1afffd310068f752b05b263100
Daniel Rori <febiolalangitan@gmail.com>
2022-11-15 22:37:42 +0800

    first commit

---
+++ Router/KomponenPcRoute.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+    getKomponen,
+    getKomponenById,
+    createKomponen,
+    updateKomponen,
+    deleteKomponen,
+} from '../tugas-ketiga-kelas-a-crypto/controller/KomponenPc.js';
+
+const router = express.Router();
+
+
+router.get('/komponens', getKomponen);
+router.get('/komponen/:id', getKomponenById);
+router.post('/komponen', createKomponen);
+router.patch('/komponen/:id', updateKomponen);
+router.delete('/komponen/:id', deleteKomponen);
+
+export default router;
